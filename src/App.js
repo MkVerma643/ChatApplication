72b8bd0a2bdceeec326cfb73122f5553c3622c83
Mukesh Verma <mukesh.verma@neosoftmail.com>
2021-07-15 15:39:25 +0530

    begin with the changes in Project

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Chat from "./components/Chat";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import { connect } from "react-redux";
-import io from 'socket.io-client'
-import { BrowserRouter, Route , useHistory} from "react-router-dom";
-
-
+import io from "socket.io-client";
+import { BrowserRouter, Route, useHistory } from "react-router-dom";
 
 function App(props) {
-  
   return (
-    
     <>
       <nav className="navbar navbar-dark bg-dark p-2 ">
         <a className="navbar-brand" href="#">
@@ -21,20 +17,24 @@ function App(props) {
         </a>
       </nav>
       <BrowserRouter>
-        <Route exact path="/chat" component={ props.isloggedin? Chat : Login }></Route>
-        <Route exact path="/" component={Login}></Route>
+        <Route
+          exact
+          path="/chat"
+          component={props.isloggedin ? Chat : Login}
+        ></Route>
+        <Route exact path="/ChatApplication" component={Login}></Route>
       </BrowserRouter>
       {/* <Home/> */}
-        {/* {
+      {/* {
           props.isloggedin?  <Home/> :  <Login/> 
         } */}
     </>
   );
 }
 
-export default connect(function(state,props){
-  console.log("state",state)
+export default connect(function (state, props) {
+  console.log("state", state);
   return {
-    isloggedin: state?.isloggedin
-  }
+    isloggedin: state?.isloggedin,
+  };
 })(App);
