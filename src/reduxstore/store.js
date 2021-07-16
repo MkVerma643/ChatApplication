ccaae0a3e3c286af57dcead2a6c244a62eb237aa
Mukesh Verma <mukesh.verma@neosoftmail.com>
2021-07-16 12:40:29 +0530

    ready to deploy

--- src/reduxstore/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'
-import reducer from './reducer'
-import {logger} from "./middleware"
-import thunk from 'redux-thunk'
-
-var middleware = applyMiddleware(logger, thunk)
-
-export default createStore(reducer,middleware)
